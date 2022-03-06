ec7d46452638d89f4df8b34405b27dcbf4fe7b0a
santiagoMoreno73 <santimor2009@hotmail.com>
2022-03-06 11:54:42 -0500

    [add]: config routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// route
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./Layout/Layout";
+
+import FormAditionalData from "./components/Forms/FormAditionalData";
+import FormRegister from "./components/Forms/FormRegister";
+import FormSignIn from "./components/Forms/FormSignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<FormSignIn />} />
+          <Route path="/register" element={<FormRegister />} />
+          <Route path="/register/aditionalData" element={<FormAditionalData />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
